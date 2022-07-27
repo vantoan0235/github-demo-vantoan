7aaa86924adda1bc6900332c046cbfde91c00d17
Ton <vantoan.contact@gmail.com>
2022-07-27 15:01:00 +0700

    Add index.html script.js file

---
+++ script.js
@@ -0,0 +1,8 @@
+var intro = "Hello World";
+    document.getElementById("intro").innerHTML = intro;
+
+// Sự kiện onchange
+function do_something() {
+    var selectedValue = document.getElementById("mySelect").value;
+    alert(selectedValue);
+}
